5eef1bd150a4f5c352934ae36b2c96a3071eb516
Isaiah Harville <harvillerisaiah@gmail.com>
2024-08-23 08:37:20 +0000

    tons

---
+++ src/ai/BasicModule.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import lightning.pytorch as pl
+
+class Recurrent(pl.LightningModule):
+    def __init__(self, in_features, hidden_size, out_features, lr=0.001):
+        super(Recurrent, self).__init__()
+        self.save_hyperparameters()
+        self.rnn = nn.RNN(in_features, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, out_features)
+        self.criterion = nn.CrossEntropyLoss()
+        self.lr = lr
+
+    def forward(self, x):
+        h0 = torch.zeros(1, x.size(0), self.hparams.hidden_size).to(x.device)
+        out, _ = self.rnn(x, h0)
+        out = self.fc(out[:, -1, :])
+        return out
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        outputs = self(x)
+        loss = self.criterion(outputs, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        pass
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        outputs = self(x)
+        loss = self.criterion(outputs, y)
+        acc = (outputs.argmax(dim=1) == y).float().mean()
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.lrw)

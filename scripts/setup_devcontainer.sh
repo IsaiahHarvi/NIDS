ccbbf5026139b81ed2efb66e2c13c1a13954bc7d
Isaiah Harville <harvillerisaiah@gmail.com>
2024-08-23 15:40:43 +0000

    create cpu requirements

--- scripts/setup_devcontainer.sh
+++ scripts/setup_devcontainer.sh
@@ -2,5 +2,10 @@
 
 bash ./scripts/setup_common.sh
 
-pip3 install --no-cache-dir --user -r .devcontainer/requirements.txt -e .
-
+if command -v nvidia-smi &> /dev/null; then
+    echo "Installing GPU requirements"
+    pip3 install --no-cache-dir --user -r .devcontainer/requirements.gpu.txt -e .
+else
+    echo "No NVIDIA GPU found... installing CPU requirements."
+    pip3 install --no-cache-dir --user -r .devcontainer/requirements-cpu.txt -e .
+fi

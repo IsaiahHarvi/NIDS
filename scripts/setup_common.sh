47900b36224894c84d4eee104e91a996e8c509ce
Isaiah Harville <harvillerisaiah@gmail.com>
2024-08-23 08:37:20 +0000

    tons

--- scripts/setup_common.sh
+++ scripts/setup_common.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -e
+#!/usr/bin/env bash
 
 sudo apt update
 sudo apt install -y vim tmux neovim fzf dnsutils iputils-ping curl
---
+++ src/ai/train.py
@@ -0,0 +1,44 @@
+import os
+import lightning.pytorch as pl
+import click
+from dvclive.lightning import DVCLiveLogger
+
+from ai.DataModule import DataModule
+from ai.BasicModule import Recurrent
+
+@click.command()
+@click.option("--epochs", default=10)
+def main(epochs):
+    train(epochs)
+    
+def train(epochs):
+    proot = "data/CIC"
+
+    all_csvs = os.listdir(proot)
+    dm = DataModule(
+        paths=[
+            f"{proot}/Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv",
+            f"{proot}/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"   
+        ], 
+        batch_size=64, 
+        test_size=0.2)
+    dm.prepare_data()
+
+    model = Recurrent(
+        in_features=dm.train_dataset[0][0].shape[1],
+        hidden_size=128, 
+        out_features=dm.n_classes,
+        lr=0.001
+    )
+    
+    trainer = pl.Trainer(
+        max_epochs=epochs, 
+        logger=DVCLiveLogger()
+    )
+
+    trainer.fit(model, dm.train_dataloader(), dm.val_dataloader())
+    trainer.save_checkpoint(f"data/checkpoints/{model.__class__.__name__}.ckpt")
+    trainer.test(model, dm.val_dataloader())
+
+if __name__ == "__main__":
+    main()

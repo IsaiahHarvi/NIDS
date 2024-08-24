21d01c2169d991b68f5709f69fef66745d4c32f2
Isaiah Harville <harvillerisaiah@gmail.com>
2024-08-24 07:49:14 +0000

    create residual network, rework model tests, create residual network service, tweaks to training pipeline

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -7,24 +7,29 @@ from icecream import ic
 from src.grpc.utils import start_server
 from src.grpc.services_pb2 import ComponentMessage, ComponentResponse
 from src.grpc.services_pb2_grpc import ComponentStub
+
 from src.services.model.recurrent import RecurrentModel
+from src.services.model.residual import ResidualModel
+
 from src.ai.DataModule import DataModule
 
 @pytest.fixture(scope="module")
-def grpc_server():
-    server_process = multiprocessing.Process(target=start_server, args=(RecurrentModel(), 50051))
+def grpc_server(request):
+    model_class, port = request.param
+    server_process = multiprocessing.Process(target=start_server, args=(model_class(), port))
     server_process.start()
     time.sleep(1)
-    yield
+    yield port
     server_process.terminate()
     server_process.join()
 
-def test_recurrent(grpc_server):
-    with grpc.insecure_channel('localhost:50051') as channel:
+@pytest.mark.parametrize("grpc_server", [(RecurrentModel, 50051), (ResidualModel, 50052)], indirect=True)
+def test_model(grpc_server):
+    port = grpc_server
+    with grpc.insecure_channel(f'localhost:{port}') as channel:
         stub = ComponentStub(channel)
         dm = DataModule(
             paths=[
-                # f"data/CIC/Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv",
                 f"data/CIC/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"   
             ],
             batch_size=1, 
@@ -33,11 +38,12 @@ def test_recurrent(grpc_server):
 
         data, label = next(iter(dm.train_dataloader()))
 
-        # ic(data.shape) # [1, 1, 80]
         data = data.numpy().flatten().tolist() # [80] bc it has to live over gRPC
         msg = ComponentMessage(input=data)
 
         response = stub.forward(msg)
         assert isinstance(response, ComponentResponse)
-        # ic(response.output)
         assert response.output == label.item(), f"False Prediction, expected {label.item()} but got {response.output}"
+
+if __name__ == "__main__":
+    pytest.main(["-sv", "tests/unit/test_model.py"])

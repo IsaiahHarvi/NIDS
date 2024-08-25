5da3b68437a3580b08cf4ea97ab8da83a6ec385d
Isaiah Harville <harvillerisaiah@gmail.com>
2024-08-25 03:21:40 +0000

    script changes

--- scripts/setup_common.sh
+++ scripts/setup_common.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-git lfs install
+if [ -f "scripts/setup_git.personal.sh" ]; then
+    bash scripts/setup_git.personal.sh
+else
+    echo -e "\n\n------------------------------------------------------------------------"
+    echo -e "WARNING: setup_git.personal.sh not found, skipping.\nPlease create this file to streamline your git setup.\nSee scripts/setup_git.sh"
+    echo -e "------------------------------------------------------------------------\n\n"
+    sleep 5
+fi
+
 git config --global --add safe.directory /workspaces/CS-499
 git config --global pull.rebase false
 
+sudo apt update > /dev/null 2>&1 && sudo apt upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt install -y vim tmux neovim fzf \
+    dnsutils iputils-ping curl tshark argus-client argus-server wget
+
+git lfs install
+git status
 # docker container prune -f | true
 # docker image prune -f | true
 # docker volume prune -f --all | true
 docker system prune --volumes -af | true
 
-sudo apt update > /dev/null 2>&1 && sudo apt upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt install -y vim tmux neovim fzf \
-    dnsutils iputils-ping curl tshark argus-client argus-server wget
+
+echo "Bringing up Services"
+docker compose up -d
--- scripts/setup_devcontainer.sh
+++ scripts/setup_devcontainer.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-bash ./scripts/setup_common.sh
-
 if command -v nvidia-smi &> /dev/null; then
     echo -e "\n\nInstalling GPU requirements"
     pip3 install --no-cache-dir --user -r .devcontainer/requirements.txt -e .
@@ -9,5 +7,7 @@ else
     echo -e "\n\nNo NVIDIA GPU found... installing CPU requirements."
     pip3 install --no-cache-dir --user -r .devcontainer/requirements-cpu.txt -e .
 fi
+
+bash ./scripts/setup_common.sh
 
 echo "Done."

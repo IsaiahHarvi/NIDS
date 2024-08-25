eb27e14e1e60180365a8bcddd7aa978588b56938
Isaiah Harville <harvillerisaiah@gmail.com>
2024-08-25 06:28:24 +0000

    finish csv feeder

--- src/services/offline_feeder/offline_feeder.py
+++ src/services/offline_feeder/offline_feeder.py
@@ -1,8 +1,8 @@
 import grpc
 import os
 import numpy as np
+import pandas as pd
 
-from src.ai.DataModule import DataModule
 from src.grpc_.services_pb2 import ComponentMessage, ComponentResponse
 from src.grpc_.services_pb2_grpc import ComponentServicer, ComponentStub
 from src.grpc_.utils import start_server
@@ -22,33 +22,33 @@ class OfflineFeeder(ComponentServicer):
             ic("Health check")
             return ComponentResponse(output=msg.input)
 
-        dm = DataModule(
-            paths=[
-                f"data/CIC/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"   
-            ],
-            batch_size=1, 
-        )
-        dm.setup()
+        df = pd.read_csv("data/CIC/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv")        
+        df = df.select_dtypes(include=[np.number])
+        df = df.drop(["Flow_ID", "Source_IP", "Destination_IP", "Timestamp"], axis=1, errors="ignore")
+        df = df.select_dtypes(include=[float, int]).fillna(0)
+        df.replace([np.inf, -np.inf], np.nan, inplace=True)
+        df.fillna(df.mean(), inplace=True)
 
-        data, label = np.random.choice(list(dm.train_dataloader()))
-        data = data.numpy().flatten().tolist() # [80] bc it has to live over gRPC
+        sample = df.sample(n=1).iloc[0].to_numpy()
+        data = sample.flatten()
+        ic(data.shape)
 
         if self.p2p:
-            self.send_to_model(data)
+            self.send_to_model(data.tolist())
             return ComponentResponse(output=[0.])
 
-        return ComponentResponse(output=data)
+        return ComponentResponse(output=data.tolist())
 
     def send_to_model(self, data: list) -> None:
         with grpc.insecure_channel(f'{self.host}:{self.port}') as channel:
-            stub = ComponentStub(channel)
+            model = ComponentStub(channel)
             request = ComponentMessage(input=data)
-            response = stub.forward(request)
-            ic(f"Model Response: {response.output}")
+            response = model.forward(request)
+            ic(f"Model Response: {response.prediction}")
 
 
 if __name__ == "__main__":
-    p2p:bool = os.environ.get("P2P", "true").lower() == "true"
+    p2p: bool = os.environ.get("P2P", "true").lower() == "true"
     host = os.environ.get("HOST", "localhost")
     target_port = int(os.environ.get("TARGET_PORT", 50052))
 

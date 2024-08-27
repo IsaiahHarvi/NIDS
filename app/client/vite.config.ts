b5243b34bf3ec25f33a4217660e620762b2b5d9d
CaseyBramlett <casey.bramlett243@gmail.com>
2024-08-27 02:39:14 +0000

    init ui and server with routing

---
+++ app/client/vite.config.ts
@@ -0,0 +1,18 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+export default defineConfig({
+  plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});

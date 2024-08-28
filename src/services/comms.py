8689e80e6102b87ef3606023a6018b38f0da9fe9
Isaiah Harville <harvillerisaiah@gmail.com>
2024-08-28 19:46:08 +0000

    format and fix pytest

--- src/services/comms.py
+++ src/services/comms.py
@@ -2,10 +2,10 @@
 Simple method to interact with the gRPC services oustide of the test suite.
 """
 
+import time
 import click
 import grpc
 import numpy as np
-import time
 from icecream import ic
 
 from src.grpc_.services_pb2 import ComponentMessage
@@ -14,9 +14,26 @@ from src.grpc_.services_pb2_grpc import ComponentStub
 
 @click.command()
 @click.option("--port", default=50054, help="Port of the service to connect to.")
-@click.option("--interactive", "-i", is_flag=True, default=False, help="Pass in ports interactively.")
-@click.option("--live", "-l", is_flag=True, default=False, help="Continue to generate samples for services that support it.")
-@click.option("--sleep", "-s", default=7, help="Time to sleep between requests when running in live mode.")
+@click.option(
+    "--interactive",
+    "-i",
+    is_flag=True,
+    default=False,
+    help="Pass in ports interactively.",
+)
+@click.option(
+    "--live",
+    "-l",
+    is_flag=True,
+    default=False,
+    help="Continue to generate samples for services that support it.",
+)
+@click.option(
+    "--sleep",
+    "-s",
+    default=7,
+    help="Time to sleep between requests when running in live mode.",
+)
 def main(port: int, interactive: bool, live: bool, sleep: int) -> None:
     if interactive:
         while True:
@@ -31,8 +48,7 @@ def connect(port: int, live: bool, sleep: int = 7) -> None:
             # Connect to Model Services
             with grpc.insecure_channel(f"localhost:{port}") as channel:
                 stub = ComponentStub(channel)
-                request = ComponentMessage(
-                    input=[1.0, 2.0, 3.0], health_check=True)
+                request = ComponentMessage(input=[1.0, 2.0, 3.0], health_check=True)
                 response = stub.forward(request)
                 ic(response.output)
         case 50053 | 50054 | 50055:
@@ -48,7 +64,8 @@ def connect(port: int, live: bool, sleep: int = 7) -> None:
                 while True:
                     stub = ComponentStub(channel)
                     request = ComponentMessage(
-                        input=[np.random.uniform(1, 9000) for _ in range(80)])
+                        input=[np.random.uniform(1, 9000) for _ in range(80)]
+                    )
                     response = stub.forward(request)
                     # ic(response.output)
                     if not live:

8906554153761f76126608e5da2842ab19000795
CaseyBramlett <casey.bramlett243@gmail.com>
2024-08-29 01:48:05 +0000

    create rest call templates and some api functions

---
+++ app/client/src/middleware/rest-calls/rest-get.ts
@@ -0,0 +1,22 @@
+import { baseUrl } from "../../lib/utils";
+
+export async function restGet(path: string) {
+  try {
+    const response = await fetch(baseUrl + path, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}

73a6b455b73fca38597a98741acd87f8cf567fa5
Isaiah Harville <harvillerisaiah@gmail.com>
2024-09-02 19:36:39 +0000

    tweak scripts

--- scripts/setup_network.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! docker network inspect services >/dev/null 2>&1; then
-    echo "Network 'services' not found. Creating it..."
-    docker network create services
-else
-    echo "Network 'services' already exists."
-fi

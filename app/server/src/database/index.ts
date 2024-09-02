0c7c9c2fc79bdebbcd4732d5f0f8f7fbbf60d9b4
Isaiah Harville <harvillerisaiah@gmail.com>
2024-09-02 05:27:28 +0000

    dsjf;asdjfsadkjhfglkjsdfhgl;kjshdjfg;lkjsdf;lgkjsdf;lkgjsd;foiguj;sldfjg;lksdjfglksdfg

--- app/server/src/database/index.ts
+++ app/server/src/database/index.ts
@@ -2,35 +2,21 @@ import { MongoClient, Db } from "mongodb";
 import * as pc from "picocolors";
 import { setupClientDb } from "./setup-databases";
 
-// const MONGO_URL = "mongodb://root:pass@host.docker.internal:27017/";
-// const MONGO_URL = "mongodb://host.docker.internal:27017/";
-// const MONGO_URL = "mongodb://root:example@mongo:27017/";
 // const MONGO_URL = "mongodb://root:example@localhost:27017/?authSource=admin";
-// const MONGO_URL = "mongodb://root:example@host.docker.internal:27017/";
-const MONGO_URL =
-  "mongodb://root:example@mongo:27017/?replicaSet=rs0&authSource=admin";
-// const MONGO_URL =
-//   "mongodb://root:example@host.docker.internal:27017/?authSource=admin";
-// const MONGO_URL = "mongodb://root:example@host.docker.internal:27017/";
-// const MONGO_URL = "mongodb://root:pass@localhost:27017/?authSource=admin";
-// const MONGO_URL =
-//   "mongodb://root:example@localhost:27017/?replicaSet=rs0&authSource=admin";
-// // const MONGO_URL = "mongodb://root:pass@host.docker.internal:27017/";
+const MONGO_URL = "mongodb://root:example@mongo:27017/";
 
 let client: MongoClient;
 export let clientDb: Db;
 export let componentsDb: Db;
 export let storeServiceDb: Db;
+
 const connectToDatabase = async () => {
   try {
     client = new MongoClient(MONGO_URL);
     await client.connect();
-
-    clientDb = client.db("client");
-    componentsDb = client.db("components");
     storeServiceDb = client.db("store_service");
 
-    setupClientDb(clientDb);
+    // setupClientDb(clientDb);
     console.log(pc.green("Success: MongoDB Connected"));
   } catch (error) {
     console.log(pc.red("Error: MongoDB Connection Failed"), error);
--- deploy/mongo/init_replset.js
+++ deploy/mongo/init_replset.js
@@ -1,9 +1,7 @@
-print("starting mongo init");
-
 try {
-  print("init replica set");
+  sleep(8000);
+  print("INITIALIZING REPLICA SET...");
 
-  // Initialize the replica set
   const result = rs.initiate({
     _id: "rs0",
     members: [
@@ -13,22 +11,16 @@ try {
       },
     ],
   });
-
+  print("REPLICA SET INITIALIZED SUCCESSFULLY!");
   printjson(result);
 
   sleep(4000);
   sleep(4000);
-  const adminDb = db.getSiblingDB("admin");
-  adminDb.createUser({
-    user: "test",
-    pwd: "test",
-    roles: [{ role: "root", db: "admin" }],
-  });
 
   adminDb.createUser({
     user: "root",
     pwd: "example",
-    roles: ["root"],
+    roles: [{ role: "root", db: "admin" }]
   });
 
   print("init script compleeted");
@@ -38,35 +30,3 @@ try {
   print("ERROR INITIALIZING REPLICA SET:");
   printjson(e);
 }
-
-// try {
-//   var config = {
-//     _id: "rs0",
-//     members: [
-//       {
-//         _id: 0,
-//         host: "mongo:27017",
-//       },
-//     ],
-//   };
-//   var result = rs.initiate(config);
-//   printjson(result);
-
-//   // If the initiation fails, try to force reconfig
-//   if (result.ok !== 1) {
-//     print("Initiation failed, forcing config.");
-//     result = rs.reconfig(config, { force: true });
-//     printjson(result);
-//   }
-
-//   sleep(5000);
-//   adminDb = db.getSiblingDB("admin");
-//   adminDb.createUser({
-//     user: "root",
-//     pwd: "pass",
-//     roles: ["root"],
-//   });
-// } catch (e) {
-//   print("ERROR:");
-//   printjson(e);
-// }

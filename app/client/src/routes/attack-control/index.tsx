80314ff87977546f7321a21479eb0d220066b506
CaseyBramlett <casey.bramlett243@gmail.com>
2024-09-04 05:23:13 +0000

    wip

--- app/client/src/routes/attack-control/index.tsx
+++ app/client/src/routes/attack-control/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import AttackControl from "../../components/AttackControl";
+import AttackControl from "../../components/pages/AttackControl";
 
 export const Route = createFileRoute("/attack-control/")({
   component: () => (

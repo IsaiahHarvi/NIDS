5580c0597c64a195bf5a88de34e32f202530056f
Isaiah Harville <harvillerisaiah@gmail.com>
2024-09-08 05:57:04 +0000

    add uninstall script

---
+++ scripts/uninstall.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# NOTE: This is for use in a production environment on a Linux machine.
+
+if grep -q "# NIDS" /etc/hosts; then
+    sudo sed -i '/# NIDS/,+1d' /etc/hosts
+    echo "NIDS entry removed from /etc/hosts"
+fi
+
+docker-compose --profile feeder --profile gui down
+docker kill $(docker ps -q) | true
+docker system prune --volumes -af | true
+docker volume rm mongo-data -f | true
+
+echo "Cleared Docker Environment and Removed NIDS Hosts Entry."

d241cefd309bf044cf4ec75b1005035cd141504a
Jacob <62169030+link2427@users.noreply.github.com>
2024-09-10 13:36:16 -0500

    Update logger to output to mongo

--- src/services/logger/compose_logger.py
+++ src/services/logger/compose_logger.py
@@ -1,16 +1,20 @@
 import os
 import docker
 from threading import Thread
-
+from pymongo import MongoClient
+from datetime import datetime
 from icecream import ic
 
 ic.configureOutput(includeContext=False, prefix="")
-
 
 class Logger:
     def __init__(self):
         ic(f"Logger started on port {os.environ.get('PORT')}")
         self.client = docker.from_env()
+        self.mongo_client = MongoClient("mongodb://root:example@mongo:27017/?replicaSet=rs0")
+        self.db = self.mongo_client["logging_service"]
+        self.collection = self.db["logs"]
+
         self.services = self.get_service_names()
         ic(f"Collecting logs for: {', '.join(self.services)}")
         self.start_log_collection()
@@ -32,13 +36,9 @@ class Logger:
     def start_log_collection(self):
         threads = []
         for name in self.services:
-            thread = Thread(target=self.monitor_logs, args=(name,))
+            thread = Thread(target=self.monitor_logs, args=(name,), daemon=True)
             thread.start()
             threads.append(thread)
-
-        # Join threads to ensure they run indefinitely
-        for thread in threads:
-            thread.join()
 
     def monitor_logs(self, name):
         spc = max([len(name) for name in self.services])
@@ -46,12 +46,26 @@ class Logger:
             ic(f"Starting log collection for service: {name}")
             container = self.client.containers.get(name)
             for line in container.logs(stream=True):
-                out = f"{name}{' ' * (spc - len(name))}| {line.strip().decode('utf-8')}"
-                # ic(out)
+                log_message = line.strip().decode('utf-8', errors='replace')
+                out = f"{name}{' ' * (spc - len(name))}| {log_message}"
+                
+                self.store_log(name, log_message)
         except Exception as e:
             ic(f"Error occurred while monitoring {name}: {e}")
         except KeyboardInterrupt:
             ic(f"SIGINT received, shutting down {name}...")
+
+    def store_log(self, service_name, message):
+        try:
+            log_entry = {
+                "service_name": service_name,
+                "message": message,
+                "timestamp": datetime.utcnow(),
+            }
+            self.collection.insert_one(log_entry)
+            ic(f"Log stored for {service_name}")
+        except Exception as e:
+            ic(f"Failed to store log: {e}")
 
 
 if __name__ == "__main__":

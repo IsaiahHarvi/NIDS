ab448e15791b6ffb7fe86a2665b131da63e9b785
Isaiah Harville <harvillerisaiah@gmail.com>
2024-12-06 01:05:08 +0000

    init sdk

--- scripts/start_common.sh
+++ scripts/start_common.sh
@@ -3,9 +3,7 @@
 docker-compose --profile gui down
 
 echo "-------------------------------------------------------------------"
-echo "Starting Services..."
 
-docker compose --profile gui up --build -d
 echo "-----------------------------------------------------------------------"
 echo "View GUI at http://localhost:8000"
 echo -e "-----------------------------------------------------------------------\n"
@@ -14,3 +12,8 @@ echo -e "-----------------------------------------------------------------------
 echo "-----------------------------------------------------------------------"
 echo "Open Terminal Interface with the 'python3 scripts/display.py'"
 echo "-----------------------------------------------------------------------"
+
+read -p "Start micro-services? (y/n): " start
+if [[ "$start" == "y" ]]; then
+    docker compose --profile gui up --build
+fi

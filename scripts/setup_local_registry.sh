c8096ddfe180a56467b1e46a425aecf104516748
isaiahharvi <harvillerisaiah@gmail.com>
2025-02-13 18:54:44 +0000

    update reqs and other tweaks

--- scripts/setup_local_registry.sh
+++ scripts/setup_local_registry.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# curl -X GET http://172.17.0.1:42069/v2/_catalog
+docker run -d -p 172.17.0.1:42069:5000 --name registry registry:2
 
 docker compose --profile gui build
+
 docker compose --profile gui push

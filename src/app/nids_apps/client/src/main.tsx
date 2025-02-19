0b001e0b96d17e839869da46a52826e7bd9ab58a
CaseyBramlett <casey.bramlett243@gmail.com>
2025-02-19 05:31:02 +0000

    WIP somewhat

--- src/app/nids_apps/client/src/main.tsx
+++ src/app/nids_apps/client/src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "./styles/index.css";
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+import { ThemeProvider } from "./components/theme-provider";
+import { StrictMode } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import { TooltipProvider } from "./components/ui/tooltip";
+// import { Topbar } from "./components/topbar";
+// import { Navbar } from "./components/navbar";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient();
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <TooltipProvider>
+            <RouterProvider router={router} />
+          </TooltipProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
--- src/app/nids_apps/client/vite.config.ts
+++ src/app/nids_apps/client/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
